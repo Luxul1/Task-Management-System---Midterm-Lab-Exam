eef9e4f9f6c84c01c2c57dd6635d3553e127d3f1
Jojo <joiamae.ruma@gmail.com>
2026-03-15 02:00:51 +0800

    Added Users & Project Categories

---
+++ Database/database.sql
@@ -0,0 +1,22 @@
+
+INSERT INTO users (name, email) VALUES
+('Joiamae', 'jsruma@ccc.edu.ph.'),
+('Jane', 'almendra@ccc.edu.ph.'),
+('Christian', 'pujalte@ccc.edu.ph.'),
+('Princess', 'garingarao@ccc.edu.ph.'),
+('Justin', 'habacon@ccc.edu.ph.'),
+('Kezzia', 'montano@ccc.edu.ph.'),
+('Robert', 'cortez@ccc.edu.ph.');
+
+INSERT INTO projects (project_name) VALUES
+('Database Design'),
+('Model Development'),
+('CRUD API Development'),
+('Relationship API Development'),
+('Data Analytics API Development'),
+('Documenting & Testing');
+
+
+
+
+
